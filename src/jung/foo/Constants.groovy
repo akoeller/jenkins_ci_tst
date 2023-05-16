365a80b1f2799d98086861adb8dd5cbad40aca6d
Arne Koeller <a.koeller@jung.de>
2023-05-16 09:40:52 +0200

    jjojo

---
+++ src/jung/foo/Constants.groovy
@@ -0,0 +1,5 @@
+package jung.foo 
+
+class Constants {
+    public static final String TEST = "TESTTTT"
+}
--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,6 @@
-def call(Map config = [:]){
-  sh "echo Hello ${config.name}, today is ${config.dayOfWeek}"
+import jung.foo.Constants
+
+def call(Map config = [:]) {
+	sh "echo Hello ${config.name}, today is ${config.dayOfWeek}"
+	sh "echo ${Constants.TEST}"
 }
