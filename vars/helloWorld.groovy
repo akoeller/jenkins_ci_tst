a99f06860f74a1b97d13a59915107028b5f31991
Arne Koeller <a.koeller@jung.de>
2023-05-16 09:48:47 +0200

    jjojo

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,11 @@
 import jung.foo.Constants
+import jung.foo.MyCustomSteps
 
 def call(Map config = [:]) {
 	sh "echo Hello ${config.name}, today is ${config.dayOfWeek}"
 	sh "echo ${Constants.TEST}"
+
+	def x = new MyCustomSteps()
+	x.execute()
+	
 }
