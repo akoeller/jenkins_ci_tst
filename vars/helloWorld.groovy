140dc7b8ef17d8e9a07461204bf3144c1ba83a5f
Arne Koeller <a.koeller@jung.de>
2023-05-16 10:30:04 +0200

    jojo

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,8 +1,12 @@
 import jung.foo.Constants
+import jung.foo.DockerEcr
 
 
 def call(Map config = [:]) {
 	sh "echo Hello ${config.name}, today is ${config.dayOfWeek}"
 	sh "echo ${Constants.TEST}"
+
+	def dockerEcr = new DockerEcr()
+	dockerEcr.buildDockerImage("joo")
 
 }
